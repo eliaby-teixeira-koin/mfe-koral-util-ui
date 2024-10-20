e9324b296d23156b167e6b061a46c6315d703472
eliabyteixeira <eliaby.teixeira@gmail.com>
2024-10-20 15:18:42 -0300

    add button component

---
+++ src/components/atoms/Button/consts.ts
@@ -0,0 +1,110 @@
+export const backgrounds = {
+   'primary': 'gray-900',
+   'secondary-gray': 'gray-100',
+   'secondary-color': 'brand-700',
+   'tertiary-gray': 'base-white',
+   'tertiary-color': 'base-white',
+   'link-gray': 'base-white',
+   'link-color': 'base-white',
+};
+
+export const colors = {
+   'primary': 'base-white',
+   'secondary-gray': 'gray-700',
+   'secondary-color': 'base-white',
+   'tertiary-gray': 'gray-600',
+   'tertiary-color': 'brand-700',
+   'link-gray': 'gray-600',
+   'link-color': 'brand-700',
+};
+
+export const bordercolors = {
+   'primary': 'gray-900',
+   'secondary-gray': 'base-white',
+   'secondary-color': 'brand-700',
+   'tertiary-gray': 'base-white',
+   'tertiary-color': 'base-white',
+   'link-gray': 'base-white',
+   'link-color': 'base-white',
+};
+
+export const paddings = {
+   sm: '0.5rem 0.75rem',
+   md: '0.625rem 0.875rem',
+   lg: '0.625rem 1rem',
+   xl: '0.75rem 1.125rem',
+   xxl: '1rem 1.375rem',
+};
+
+export const widths = {
+   sm: '9.625rem', // 154px
+   md: '9.875rem', // 158px
+   lg: '11.0625rem', // 177px
+   xl: '11.3125rem', // 181px
+   xxl: '13.5rem', // 216px
+};
+
+export const heights = {
+   sm: '2.25rem', // 36px
+   md: '2.5rem', // 40px
+   lg: '2.75rem', // 44px
+   xl: '3rem', // 48px
+   xxl: '3.75rem', // 60px
+};
+
+export const fontsizes = {
+   sm: '0.875rem', // 14px
+   md: '0.875rem', // 14px
+   lg: '1rem', // 16px
+   xl: '1rem', // 16px
+   xxl: '1.125rem', // 18px
+};
+
+export const hovercolors = {
+   'primary': 'base-white',
+   'secondary-gray': 'gray-800',
+   'secondary-color': 'base-white',
+   'tertiary-gray': 'gray-700',
+   'tertiary-color': 'brand-800',
+   'link-gray': 'gray-700',
+   'link-color': 'brand-800',
+};
+
+export const hoverbackgrounds = {
+   'primary': 'gray-800',
+   'secondary-gray': 'gray-200',
+   'secondary-color': 'brand-800',
+   'tertiary-gray': 'gray-50',
+   'tertiary-color': 'brand-50',
+   'link-gray': 'base-white',
+   'link-color': 'base-white',
+};
+
+export const hoverbordercolors = {
+   'primary': 'gray-900',
+   'secondary-gray': 'gray-200',
+   'secondary-color': 'brand-800',
+   'tertiary-gray': 'base-white',
+   'tertiary-color': 'brand-50',
+   'link-gray': 'base-white',
+   'link-color': 'base-white',
+};
+
+export const hoverboxshadows = {
+   'primary': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(152, 162, 179, 0.14)',
+   'secondary-gray': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(152, 162, 179, 0.14)',
+   'secondary-color': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(67, 228, 101, 0.24)',
+   'tertiary-gray': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(152, 162, 179, 0.14)',
+   'tertiary-color': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(152, 162, 179, 0.14)',
+   'link-gray': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(152, 162, 179, 0.14)',
+   'link-color': '0px 1px 2px 0px rgba(16, 24, 40, 0.05), 0px 0px 0px 4px rgba(152, 162, 179, 0.14)',
+};
+
+// when iconOnly
+export const paddingsIconOlny = {
+   sm: '0.5rem',
+   md: '0.625rem',
+   lg: '0.75rem',
+   xl: '0.875rem',
+   xxl: '1rem',
+};
--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,9 +1,28 @@
-import "./styles.css";
+import { ButtonContainer }  from './styles';
+import { ButtonProps } from './props';
 
-export function Button({ children }) {
-  return (
-    <div className="container-button">
-      <button type="button">{children}</button>
-    </div>
-  );
-};
+export function Button({
+   text,
+   variant,
+   size,
+   icon,
+   iconOnly,
+   onClick,
+   iconStart,
+   iconEnd
+}: ButtonProps) {
+   console.log(iconOnly);
+   return (
+      <ButtonContainer
+         variant={variant}
+         onClick={onClick}
+         size={size}
+         iconOnly={iconOnly}
+      >
+         {iconOnly ? icon : null}
+         {iconOnly ? null : iconStart}
+         {iconOnly ? null : <label>{text} {size}</label>}
+         {iconOnly ? null : iconEnd}
+      </ButtonContainer>
+   )
+}
